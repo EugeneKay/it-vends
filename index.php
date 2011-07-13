637ea2d9978e6c57b728706b39750eff0226b121
Eugene E. Kashpureff <eugenekay@kashpureff.org>
2011-07-13 17:58:06 -0500

    Updated jQuery, removed extraneous files, organized css/js/imgs into subdirs.

--- index.php
+++ index.php
@@ -1,28 +1,29 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
-    <head>
-        <meta http-equiv="Content-type" content="text/html; charset=UTF-8"/>
-        <title>It Vends!</title>
-        <link rel="stylesheet" type="text/css" href="css/itvends.css"/>
-        <link rel="stylesheet" type="text/css" href="css/start/jquery-ui-1.8.5.custom.css"/>
-        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
-        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.5/jquery-ui.min.js"></script>
-        <script type="text/javascript">
-          $(document).ready(function() {
-                $( "#vendbutton" ).button();
-                $( "#vendbutton" ).click(function() { $('#awesome').submit(); });
-          });
-        </script>
-    </head>
-    <body><center>
-        <div id="vendit">
-            <form action="." id="awesome" method="post">
-                <input name="action" value="vend" type="hidden"/>
-                <!-- <input id="vendbutton" value="Vend!" type="submit"/> -->
-                <a href="#" id="vendbutton"><span style="font-size:100px;">Vend!</span></a>
-            </form>
-            <center>
+	<head>
+		<meta http-equiv="Content-type" content="text/html; charset=UTF-8"/>
+		<title>It Vends!</title>
+		<base href="http://itvends.com/" />
+		<link rel="stylesheet" type="text/css" href="css/itvends.css"/>
+		<link rel="stylesheet" type="text/css" href="css/start/jquery-ui-1.8.5.custom.css"/>
+		<script type="text/javascript" src="js/jquery-1.6.2.min.js"></script>
+		<script type="text/javascript" src="js/jquery-ui-1.8.14.js"></script>
+		<script type="text/javascript">
+		  $(document).ready(function() {
+				$( "#vendbutton" ).button();
+				$( "#vendbutton" ).click(function() { $('#awesome').submit(); });
+		  });
+		</script>
+	</head>
+	<body><center>
+		<div id="vendit">
+			<form action="." id="awesome" method="post">
+				<input name="action" value="vend" type="hidden"/>
+				<!-- <input id="vendbutton" value="Vend!" type="submit"/> -->
+				<a href="#" id="vendbutton"><span style="font-size:100px;">Vend!</span></a>
+			</form>
+			<center>
 <?php
 //
 // IT VENDS IT VENDS IT VENDS IT VENDS IT VENDS IT VENDS IT VENDS IT VENDS IT VENDS IT VENDS IT VENDS IT VENDS IT VENDS
@@ -36,8 +37,8 @@ switch (@$_POST["action"]) {
 	default:
 	}
 ?>
-            </center>
-        </div>
-        </center>
-    </body>
+			</center>
+		</div>
+		</center>
+	</body>
 </html>
