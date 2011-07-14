ec8a196c662fc0214fdb654f9a3420cfebc3fae2
Eugene E. Kashpureff <eugenekay@kashpureff.org>
2011-07-13 20:46:27 -0500

    Added some more Actions.

--- vend.php
+++ vend.php
@@ -3,17 +3,23 @@
 
 require_once( "vendlist.php" );
 if (isset( $_POST["action"] ) ) {
-	$action = $_GET["action"];
+	$action = $_POST["action"];
 }
 elseif (isset( $_GET["action"] ) ) {
-	$action = $_POST["action"];
+	$action = $_GET["action"];
 }
 else {
 	$action = "vend";
 }
 switch ($action) {
-	case "vend":
-		echo $vendlist[array_rand($vendlist, 1)];
-		break;
-	default:
+case "give":
+	echo "Item giving is currently not supported. Sorry";
+	break;
+case "inventory":
+	echo implode(", ", $vendlist);
+	break;
+case "vend":
+default:
+	echo $vendlist[array_rand($vendlist, 1)];
+	break;
 }
