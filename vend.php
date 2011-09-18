a151311d8ec3c6f035edfd6f04e2e5d585e0416a
William Cahill-Manley <william@cahillmanley.com>
2011-09-17 22:19:00 -0700

    Added format checker

--- vend.php
+++ vend.php
@@ -6,6 +6,10 @@
 // Consult the README file included with this program for License information.
 //
 require_once( "vendlist.php" );
+
+$format = array_key_exists( 'format', $_GET) ? $_GET['format'] : 'text';
+$format = in_array($format, $formats) ? $format : 'text';
+
 if (isset( $_POST["action"] ) ) {
 	$action = $_POST["action"];
 }
