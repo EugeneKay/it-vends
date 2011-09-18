8b1d89d844eb476f7efef9f769b4c1f9e0221794
Eugene E. Kashpureff <eugene@kashpureff.org>
2011-09-18 18:53:53 -0500

    Enhance cross-domain compatibility
    *Alter <base href=""> to refer to the server's HTTP_HOST variable. This may cause issues with non-Apache servers.

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ default:
 	<head>
 		<meta http-equiv="Content-type" content="text/html; charset=UTF-8"/>
 		<title>It Vends!</title>
-		<base href="<?php echo PROTOCOL;?>://itvends.com/"/>
+		<base href="<?php echo PROTOCOL;?>://<?php echo $_SERVER["HTTP_HOST"]?>/"/>
 		<link rel="icon" type="image/png" href="favicon.ico"/>
 		<link rel="stylesheet" type="text/css" href="css/itvends.css"/>
 		<link rel="stylesheet" type="text/css" href="css/start/jquery-ui-1.8.5.custom.css"/>
