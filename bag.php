ef7e98075a981e995081aa6df709f25034c69535
Eugene E. Kashpureff Jr <eugene@kashpureff.org>
2013-09-28 00:30:21 +0000

    Add bag page

---
+++ bag.php
@@ -0,0 +1,19 @@
+<?php
+
+$bag[]="bag";
+$bag[]="box";
+$bag[]="bowl";
+$bag[]="basket";
+$bag[]="bundle";
+$bag[]="bottle";
+$bag[]="basin";
+$bag[]="barrel";
+$bag[]="bucket";
+$bag[]="bin";
+$bag[]="bunch";
+$bag[]="boatload";
+$bag[]="bouquet";
+$bag[]="bushel";
+
+echo $bag[array_rand($bag)];
+?>
